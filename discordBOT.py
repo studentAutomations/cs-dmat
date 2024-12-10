d33b16d7b76abdde38be2fb0fc3f7c94f18704d2
studentAutomations <student.automations@gmail.com>
2024-12-10 11:00:10 +0100

    Update discordBOT.py

--- discordBOT.py
+++ discordBOT.py
@@ -8,7 +8,7 @@ for url in WEBHOOK_URL:
     hook = Webhook(url)
 
     embed = Embed(
-        description="**[DISKRETNA-MAT forum link - click here -](https://cs.elfak.ni.ac.rs/nastava/mod/forum/view.php?id=4587)**",
+        description="**[DISKRETNA-MAT forum link - click here -](https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=97&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2000&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=)**",
         color=0x3498DB
     )
 
